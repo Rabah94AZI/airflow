b65280279e3b5435668d3e120fb7076aefad2ddd
thaninaESGI <thanina.A1996@gmail.com>
2024-06-27 14:32:41 +0200

    test

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ import os
 api_key = os.getenv('API_KEY')
 
 if api_key:
-    print(f"API Key: {api_key}")
+    print(f"API Key for our khra project : {api_key}")
 else:
     print("API Key not found!")
